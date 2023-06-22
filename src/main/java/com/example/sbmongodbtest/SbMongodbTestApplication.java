5aea075d7206167bd4ae5bf3c164e0529f1dafa2
nazarchykur <nazarchykur@gmail.com>
2023-06-22 10:41:25 +0300

    init project and added:  - props  - Dockerfile  - docker-compose.yml

---
+++ src/main/java/com/example/sbmongodbtest/SbMongodbTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.sbmongodbtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbMongodbTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbMongodbTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sbmongodbtest/SbMongodbTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sbmongodbtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbMongodbTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
